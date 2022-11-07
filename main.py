0db0821d0c62643cb1361564d6dab8b0c1e09a8c
Vaibhav-Rawat03 <111760496+Vaibhav-Rawat03@users.noreply.github.com>
2022-11-07 12:13:02 +0530

    Greatest no

---
+++ main.py
@@ -0,0 +1,11 @@
+
+def git(a,b,c):
+    if a >= b and a >= c:
+        print("The greatest no. is ", a)
+    elif b > a and b > c:
+        print("the greatest no. is ", b)
+    else:
+        print("The greatest no. is ", c)
+
+
+git(10, 20, 30)
